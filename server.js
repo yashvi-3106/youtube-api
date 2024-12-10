a204b30bde8a1091a990f21335e91be5b183261f
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2024-12-10 23:09:15 +0530

    done

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5175'],  // Add both ports here
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  };
+  
+  app.use(cors(corsOptions));
+
 // app.use(cors({
 //     origin: 'http://localhost:5173',
 //   }));
 
-app.use(cors({
-    origin: '*'
-  }));
+// app.use(cors({
+//     origin: '*'
+//   }));
 
 let youtube =
     [
@@ -42,6 +50,30 @@ let youtube =
         { id: 12, title: "Computer.." }
     ]
 
+    const data = [
+        {id:1, img:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%204%20(5).png", title:'Nadir On The Top'},
+        {id:2, img:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%201%20(1).png", title:'Coke Studio Bangla'},
+        {id:3, img:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%201%20(2).png", title:'MKBHD'},
+        {id:4, img:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%201%20(3).png", title:'Figma'},
+        {id:5, img:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%201%20(4).png", title:'ATC Android ToTo'},
+    ]
+
+   const side = [
+      {id:1, img_url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/home.png",text:'Home'},
+      {id:2, img_url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/explore.png",text:'Explores'},
+      {id:3, img_url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/shorts.png",text:'Shorts'},
+      {id:4, img_url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/subscription.png",text:'Subscriptions'},
+   ] 
+
+   const down = [
+      {id:1, img1:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/library.png",text1:'Library'},
+      {id:2, img1:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/history.png",text1:'History'},
+      {id:3, img1:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/your_video.png",text1:'Your Videos'},
+      {id:4, img1:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/watch_later.png",text1:'Watch Later'},
+      {id:5, img1:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/liked.png",text1:'Liked Videos'},
+      {id:6, img1:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/down_arrow.png",text1:'Show More'},
+   ]
+
 
       app.get("/youtube", (req, res) => {
         res.json(youtube);
@@ -49,6 +81,18 @@ let youtube =
 
       app.get("/top", (req, res) => {
         res.json(top);
+      });
+
+      app.get("/data", (req, res) => {
+        res.json(data);
+      });
+
+      app.get("/side", (req, res) => {
+        res.json(side);
+      });
+
+      app.get("/down", (req, res) => {
+        res.json(down);
       });
 
       const PORT = 3000;
